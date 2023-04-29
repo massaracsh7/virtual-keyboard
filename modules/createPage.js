9c64a509d0c388562d2c9c9ba8754c4ceb7f9e17
Your Name <you@example.com>
2023-04-29 11:05:43 +0300

    fix: change createPage description

--- modules/createPage.js
+++ modules/createPage.js
@@ -2,7 +2,7 @@ const createPage = (lang, keyboard) => {
   const container = document.createElement('div');
   container.classList.add('container');
   const descr = document.createElement('p');
-  descr.innerHTML = 'Change language - AltRight  OS Windows';
+  descr.innerHTML = 'Change language - AltRight <br><br> Windows Operating System';
   const textarea = document.createElement('textarea');
   textarea.setAttribute('autofocus', true);
   textarea.classList.add('textarea');
